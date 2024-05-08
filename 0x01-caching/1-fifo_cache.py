d85f12c283f4c76c27fee09b14dc68419862ab3a
VickArmand <victormaina1962@gmail.com>
2024-05-08 15:24:28 +0300

    FIFO caching

---
+++ 0x01-caching/1-fifo_cache.py
@@ -0,0 +1,48 @@
+#!/usr/bin/python3
+"""
+1-fifo_cache module has a class FIFOCache
+which inherits from BaseCaching and is a caching system
+"""
+from base_caching import BaseCaching
+
+
+class FIFOCache(BaseCaching):
+    """
+    You must use self.cache_data - dictionary from the parent class BaseCaching
+    You can overload def __init__(self):
+    but don’t forget to call the parent init: super().__init__()
+    """
+    def __init__(self):
+        """initialize"""
+        super().__init__()
+
+    def put(self, key, item):
+        """
+        Must assign to the dictionary self.cache_data
+        the item value for the key key.
+        If key or item is None, nothing should be done.
+        If the number of items in self.cache_data is higher than
+        BaseCaching.MAX_ITEMS:
+        you must discard the first item put in cache
+        (FIFO algorithm)
+        you must print DISCARD: with the key discarded
+        and following by a new line
+        """
+        if key is not None or item is not None:
+            keys = sorted(self.cache_data.keys())
+            size = len(self.cache_data.keys())
+            if size == self.MAX_ITEMS and key not in keys:
+                topKey = keys[0]
+                self.cache_data.pop(topKey)
+                print("DISCARD: {}".format(topKey))
+            self.cache_data[key] = item
+
+    def get(self, key):
+        """
+        Must return the value in self.cache_data linked to key.
+        If key is None or if the key doesn’t exist in self.cache_data,
+        return None.
+        """
+        if key is None:
+            return None
+        return self.cache_data.get(key)
