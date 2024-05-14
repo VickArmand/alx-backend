99c0e67e4fed680d0c3507b6c6d2ea33020d4a72
VickArmand <victormaina1962@gmail.com>
2024-05-14 15:57:38 +0300

    0. Basic Flask app

---
+++ 0x02-i18n/0-app.py
@@ -0,0 +1,16 @@
+#!/usr/bin/env python3
+"""a basic Flask app"""
+from flask import Flask, render_template
+app = Flask(__name__)
+
+
+@app.route('/')
+def home() -> None:
+    """
+    Create a single / route and an index.html template
+    that simply outputs “Welcome to Holberton” as page title (<title>)
+    and “Hello world” as header (<h1>).
+    """
+    return render_template('index.html',
+                           title="Hello World",
+                           header="Welcome to Holberton")
