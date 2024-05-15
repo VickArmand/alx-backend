6aaac5bea77d48531cd4ad7cb840e8bfe67fcae2
VickArmand <victormaina1962@gmail.com>
2024-05-15 16:50:37 +0300

    0-corrections

--- 0x02-i18n/0-app.py
+++ 0x02-i18n/0-app.py
@@ -11,6 +11,4 @@ def home() -> str:
     that simply outputs “Welcome to Holberton” as page title (<title>)
     and “Hello world” as header (<h1>).
     """
-    return render_template('0-index.html',
-                           title="Hello World",
-                           header="Welcome to Holberton")
+    return render_template('0-index.html')
