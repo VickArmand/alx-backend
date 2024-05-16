f20182d34ef54a25a6133f0e0fd6dc579095a193
VickArmand <victormaina1962@gmail.com>
2024-05-16 10:50:33 +0300

    Parametrize templates

---
+++ 0x02-i18n/3-app.py
@@ -0,0 +1,38 @@
+#!/usr/bin/env python3
+"""Babel setup"""
+from flask import Flask, render_template, request
+from flask_babel import Babel
+app = Flask(__name__)
+babel = Babel(app)
+app.url_map.strict_slashes = False
+
+
+class Config:
+    """configuration class"""
+    LANGUAGES = ["en", "fr"]
+
+    def __init__(self) -> None:
+        """initialize by changing default language to en and timezone to utc"""
+        app.config['BABEL_DEFAULT_LOCALE'] = self.LANGUAGES[0]
+        app.config['BABEL_DEFAULT_TIMEZONE'] = 'UTC'
+        app.config.from_object(Config())
+
+
+@app.route('/')
+def home() -> str:
+    """
+    Create a single / route and an index.html template
+    that simply outputs “Welcome to Holberton” as page title (<title>)
+    and “Hello world” as header (<h1>).
+    """
+    return render_template('2-index.html')
+
+
+@babel.localeselector
+def get_locale():
+    """Get locale from request"""
+    return request.accept_languages.best_match(app.config['LANGUAGES'])
+
+
+if __name__ == '__main__':
+    app.run(host='0.0.0.0', port=5000)
